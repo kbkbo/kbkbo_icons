fe6e322ed7454ae88678ea63659bd8e0e6ddcdad
Peng90813 <111048055+Peng90813@users.noreply.github.com>
2024-02-21 23:28:01 +0800

    Update generate_image_json.py

--- .github/scripts/generate_image_json.py
+++ .github/scripts/generate_image_json.py
@@ -4,7 +4,7 @@ import json
 def generate_json():
     image_folder = 'Icons'
     json_data = {
-        "name": "Peng图标订阅",
+        "name": "Peng_Icons图标订阅",
         "description": "收集一些自己常用的图标",
         "icons": []
     }
@@ -17,7 +17,7 @@ def generate_json():
             json_data["icons"].append(icon_data)
 
     # Set the output path relative to the repository root
-    output_path = os.path.join(os.getcwd(), 'Peng.icons.json')
+    output_path = os.path.join(os.getcwd(), 'Icons.icons.json')
 
     with open(output_path, 'w', encoding='utf-8') as json_file:
         json.dump(json_data, json_file, ensure_ascii=False, indent=2)
